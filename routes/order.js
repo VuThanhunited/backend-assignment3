5958299c26339a15e99b83ab038ea842c5d55770
Vũ Thành <vtu211102000@gmail.com>
2025-05-10 22:59:42 +0700

    Fix CORS for Firebase lan 5

--- routes/order.js
+++ routes/order.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { verifyUser } = require("../middleware/authMiddleware");
+const auth = require("../middleware/auth");
 const {
   createOrder,
   getUserOrders,
   getOrderById,
 } = require("../controllers/orderController");
 
-router.post("/", verifyUser, createOrder);
+router.post("/", verifyUser, auth, createOrder);
 router.get("/user", verifyUser, getUserOrders);
 router.get("/:id", verifyUser, getOrderById);
 
